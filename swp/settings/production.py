e9667c85f237cccb1515c4924717c3d2c4e4115d
Karsten Kosmala <kosmala@cosmocode.de>
2020-12-08 09:47:44 +0100

    we're behind a reverse proxy

--- swp/settings/production.py
+++ swp/settings/production.py
@@ -3,3 +3,7 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['production.swp.cosmoco.de', 'production.swp.cosmocode.de']
+
+USE_X_FORWARDED_FOR = True
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
