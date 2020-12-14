a9ca7dd0e574aa2bfe0d9e3c43ff0d673767365e
Fabian Niemann <niemann@cosmocode.de>
2020-12-14 12:03:17 +0100

    SWP-15: Implement password reset workflow

--- swp/urls.py
+++ swp/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('', SWPView.as_view(), name='index'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/', include(([
+        path('', PasswordResetView.as_view(), name='start'),
+        path('<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='confirm'),
+        path('complete/', PasswordResetCompleteView.as_view(), name='complete'),
+        path('done/', PasswordResetDoneView.as_view(), name='done'),
+    ], 'password-reset'))),
     path('admin/', admin.site.urls),
 ]
 
--- swp/views/__init__.py
+++ swp/views/__init__.py
@@ -1,8 +1,19 @@
-from .auth import LoginView, LogoutView
+from .auth import (
+    LoginView,
+    LogoutView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+    PasswordResetDoneView,
+    PasswordResetView,
+)
 from .base import SWPView
 
 __all__ = [
     'LoginView',
     'LogoutView',
+    'PasswordResetConfirmView',
+    'PasswordResetCompleteView',
+    'PasswordResetDoneView',
+    'PasswordResetView',
     'SWPView',
 ]
