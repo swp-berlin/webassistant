1923f9c6f193b83a6e99dc60d269c95f8e7209d3
Benjamin Brauch <brauch@cosmocode.de>
2020-12-15 13:25:30 +0100

    added FIXME to scraper serizalizer

--- swp/api/v1/serializers/scraper.py
+++ swp/api/v1/serializers/scraper.py
@@ -17,4 +17,5 @@ class ScraperSerializer(ModelSerializer):
         fields = ['id', 'type', 'thinktank', 'is_active', 'data', 'start_url', 'interval', 'last_run']
 
     def save(self, **kwargs):
+        # FIXME this is a hack and should be handled differently when resolvers are created in the front-end
         return super().save(data=json.loads(self.validated_data.get('data')))
