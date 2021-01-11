d696fecc35cdc8b549d64073b400d8f03d845908
Fabian Niemann <niemann@cosmocode.de>
2021-01-11 16:23:00 +0100

    SWP-27: Let's make some kids

--- swp/assets/scripts/components/thinktank/ThinktankRow.js
+++ swp/assets/scripts/components/thinktank/ThinktankRow.js
@@ -6,17 +6,19 @@ import _ from 'utils/i18n';
 
 const DisabledLabel = _('disabled');
 
-const ThinktankLink = ({id, label, ...props}) => (
-    <Link to={`/thinktank/${id}/`} {...props}>{label}</Link>
+const ThinktankLink = ({id, children, ...props}) => (
+    <Link to={`/thinktank/${id}/`} {...props}>
+        {children}
+    </Link>
 );
 
 const ThinktankRow = ({id, name, publicationCount, scraperCount, lastRun, errorCount, isActive}) => (
     <tr className={isActive || 'disabled'}>
-        <td><ThinktankLink id={id} label={name} /></td>
-        <td className="text-right"><ThinktankLink id={id} label={publicationCount} /></td>
-        <td className="text-right"><ThinktankLink id={id} label={scraperCount} /></td>
+        <td><ThinktankLink id={id}>{name}</ThinktankLink></td>
+        <td className="text-right"><ThinktankLink id={id}>{publicationCount}</ThinktankLink></td>
+        <td className="text-right"><ThinktankLink id={id}>{scraperCount}</ThinktankLink></td>
         <td className="text-right">{isActive ? <DateTime value={lastRun} /> : DisabledLabel}</td>
-        <td className="text-right"><ThinktankLink id={id} label={errorCount} /></td>
+        <td className="text-right"><ThinktankLink id={id}>{errorCount}</ThinktankLink></td>
     </tr>
 );
 
