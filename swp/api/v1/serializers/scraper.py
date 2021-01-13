a7453056a7e1552fdcc943157f7d031741851201
Benjamin Brauch <brauch@cosmocode.de>
2021-01-13 14:36:25 +0100

    basic resolver validation

--- swp/api/v1/serializers/scraper.py
+++ swp/api/v1/serializers/scraper.py
@@ -1,12 +1,90 @@
-from rest_framework.serializers import ModelSerializer
+from collections import OrderedDict
+
+from rest_framework.fields import CharField, ChoiceField, IntegerField
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from swp.models import Scraper
+from swp.models.choices import DataResolverKey, ResolverType
 
 from .fields import ThinktankField
 
 
+class ResolverConfigSerializer(Serializer):
+    type = ChoiceField(choices=ResolverType.choices)
+
+    def get_serializer(self, type, *args, **kwargs):
+        serializer_type = ResolverSerializers[type]
+        return serializer_type(*args, **kwargs)
+
+    def to_representation(self, instance):
+        serializer = self.get_serializer(instance['type'], instance)
+
+        return {**super().to_representation(instance), **serializer.to_representation(instance)}
+
+    def to_internal_value(self, data):
+        internal_data = super().to_internal_value(data)
+
+        type = internal_data['type']
+
+        serializer = self.get_serializer(type, data=data)
+
+        return OrderedDict({**internal_data, **serializer.to_internal_value(data)})
+
+    def validate(self, data):
+        type = data['type']
+        serializer = self.get_serializer(type, data=data)
+
+        return {**data, **serializer.validate(data)}
+
+
+class PaginatorSerializer(Serializer):
+    list_selector = CharField()
+    button_selector = CharField()
+    max_pages = IntegerField(min_value=1)
+
+
+class ListResolverSerializer(Serializer):
+    selector = CharField()
+    paginator = PaginatorSerializer()
+    resolvers = ResolverConfigSerializer(many=True)
+
+
+class LinkResolverSerializer(Serializer):
+    selector = CharField()
+    resolvers = ResolverConfigSerializer(many=True)
+
+
+class DataResolverSerializer(Serializer):
+    key = ChoiceField(choices=DataResolverKey.choices)
+    selector = CharField()
+
+
+class AttributeResolverSerializer(DataResolverSerializer):
+    attribute = CharField()
+
+
+class StaticResolverSerializer(Serializer):
+    key = ChoiceField(choices=DataResolverKey.choices)
+    value = CharField()
+
+
+class DocumentResolverSerializer(Serializer):
+    selector = CharField()
+
+
+ResolverSerializers = {
+    ResolverType.LIST: ListResolverSerializer,
+    ResolverType.LINK: LinkResolverSerializer,
+    ResolverType.DATA: DataResolverSerializer,
+    ResolverType.ATTRIBUTE: AttributeResolverSerializer,
+    ResolverType.STATIC: StaticResolverSerializer,
+    ResolverType.DOCUMENT: DocumentResolverSerializer,
+}
+
+
 class ScraperSerializer(ModelSerializer):
     thinktank = ThinktankField()
+    data = ResolverConfigSerializer()
 
     class Meta:
         model = Scraper
