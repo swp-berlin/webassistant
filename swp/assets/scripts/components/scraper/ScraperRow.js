476396d26e019324145669fff2c6df978478b1c5
Fabian Niemann <niemann@cosmocode.de>
2021-01-13 19:09:38 +0100

    SWP-28: Refactor external links into component

--- swp/assets/scripts/components/scraper/ScraperRow.js
+++ swp/assets/scripts/components/scraper/ScraperRow.js
@@ -1,4 +1,5 @@
 import DateTime from 'components/DateTime';
+import ExternalLink from 'components/Navigation/ExternalLink';
 import _ from 'utils/i18n';
 
 
@@ -6,7 +7,7 @@ const DisabledLabel = _('disabled');
 
 const ScraperRow = ({id, url, type, lastRun, errorCount, isActive}) => (
     <tr className={isActive ? null : 'disabled'} data-id={id}>
-        <td><a href={url}>{url}</a></td>
+        <td><ExternalLink to={url} /></td>
         <td>{type}</td>
         <td>{isActive ? <DateTime value={lastRun} /> : DisabledLabel}</td>
         <td className="text-right">{errorCount}</td>
