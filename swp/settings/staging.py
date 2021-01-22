d55359ff8f0603cc85b11ee3833180a2861bc86d
Sebastian Manthey <manthey@cosmocode.de>
2021-01-22 11:35:00 +0100

    Set default sender address for mails

--- swp/settings/staging.py
+++ swp/settings/staging.py
@@ -8,6 +8,8 @@ configure_sentry('https://3710c408a1494bc6b43038ed20acfed5@sentry.cosmocode.de/4
 
 ALLOWED_HOSTS = ['staging.swp.cosmoco.de', 'staging.swp.cosmocode.de']
 
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', 'staging@swp.cosmocode.de')
+
 USE_X_FORWARDED_FOR = True
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
