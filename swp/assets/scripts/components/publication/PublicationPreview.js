d574560d33c3ab28e1aeeac97e6ef41d88268736
Fabian Niemann <niemann@cosmocode.de>
2021-01-27 11:55:51 +0100

    SWP-47: Shoe-horn useCallback into pagination handlers

--- swp/assets/scripts/components/publication/PublicationPreview.js
+++ swp/assets/scripts/components/publication/PublicationPreview.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useCallback, useState} from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import {Button, ButtonGroup} from '@blueprintjs/core';
@@ -20,18 +20,29 @@ const PublicationPreview = ({thinktankID, page, pageSize, noTitle, className, ..
     const [currentPage, setCurrentPage] = useState(page || 1);
 
     const endpoint = '/publication/';
-    const {loading, result} = useQuery(endpoint, {thinktank_id: thinktankID, page: currentPage, page_size: pageSize});
+    const params = {thinktank_id: thinktankID, page: currentPage, page_size: pageSize};
+    const {loading, result, success} = useQuery(endpoint, params);
 
-    if (loading) return Loading;
-    const {results: publications, next: nextPage, previous: prevPage, count} = result.data;
+    let publications = [];
+    let nextPage = null;
+    let prevPage = null;
+    let count = 0;
+    if (success) {
+        publications = result.data.results;
+        nextPage = result.data.next;
+        prevPage = result.data.previous;
+        count = result.data.count;
+    }
 
     const pages = generatePageNumbers(count, pageSize);
 
-    const handleNextPage = () => (nextPage && setCurrentPage(currentPage + 1));
-    const handlePrevPage = () => (prevPage && setCurrentPage(currentPage - 1));
-    const handleFirstPage = () => setCurrentPage(1);
-    const handleLastPage = () => setCurrentPage(pages.length);
+    const handleNextPage = useCallback(() => nextPage && setCurrentPage(currentPage + 1), [currentPage, nextPage]);
+    const handlePrevPage = useCallback(() => prevPage && setCurrentPage(currentPage - 1), [currentPage, prevPage]);
+    const handleFirstPage = useCallback(() => setCurrentPage(1), [setCurrentPage]);
+    const handleLastPage = useCallback(() => setCurrentPage(pages.length), [pages.length]);
+    const handlePage = useCallback(page => setCurrentPage(page), [setCurrentPage]);
 
+    if (loading) return Loading;
     const title = count ? interpolate(PublicationsLabel, [count], false) : NoPublications;
 
     return (
@@ -45,7 +56,7 @@ const PublicationPreview = ({thinktankID, page, pageSize, noTitle, className, ..
                     <Button key="first" onClick={handleFirstPage} disabled={!prevPage} icon="double-chevron-left" />
                     <Button key="prev" onClick={handlePrevPage} disabled={!prevPage} icon="chevron-left" />
                     {pages.map(page => (
-                        <Button key={page} onClick={() => setCurrentPage(page)} active={page === currentPage}>
+                        <Button key={page} onClick={() => handlePage(page)} active={page === currentPage}>
                             {page}
                         </Button>
                     ))}
