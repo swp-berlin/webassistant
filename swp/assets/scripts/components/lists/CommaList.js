c3f4ffe9aadc6cf3406bb7fe67206d9b20c78b46
Fabian Niemann <niemann@cosmocode.de>
2021-01-27 13:21:32 +0100

    SWP-47: Refactor CommaList component

--- swp/assets/scripts/components/lists/CommaList.js
+++ swp/assets/scripts/components/lists/CommaList.js
@@ -1,35 +1,36 @@
 import PropTypes from 'prop-types';
-import _ from 'utils/i18n';
+import Conjunctions from './Conjunctions';
 
 
-const And = _('and');
-const Or = _('or');
+const generateSeparators = (count, conjunction, delimiter = ',') => {
+    const finalSeparator = !conjunction ? delimiter : ` ${conjunction} `;
+    return new Array(count).fill('', 0, 1).fill(`${delimiter} `, 1, count - 1).fill(finalSeparator, count - 1, count);
+};
 
-const CommaList = ({items, alwaysUseComma, endWithOr, ...props}) => {
-    const last = items.length - 1;
-    const lastWord = endWithOr ? Or : And;
-    const finalSeparator = alwaysUseComma ? ',' : ` ${lastWord}`;
-
-    const chooseSeparator = i => (i < last ? ',' : finalSeparator);
+const CommaList = ({items, conjunction, delimiter, ...props}) => {
+    const separators = generateSeparators(items.length, conjunction, delimiter);
 
     return (
         <ul className="inline list-inline" {...props}>
             {items.map((item, i) => (
                 // eslint-disable-next-line react/no-array-index-key
-                <li key={i}>{i > 0 ? `${chooseSeparator(i)} ${item}` : item}</li>
+                <li key={i}>
+                    {separators[i]}
+                    {item}
+                </li>
             ))}
         </ul>
     );
 };
 
 CommaList.defaultProps = {
-    alwaysUseComma: false,
-    endWithOr: false,
+    conjunction: Conjunctions.And,
+    delimiter: ',',
 };
 
 CommaList.propTypes = {
-    alwaysUseComma: PropTypes.bool,
-    endWithOr: PropTypes.bool,
+    conjunction: PropTypes.string,
+    delimiter: PropTypes.string,
 };
 
 export default CommaList;
