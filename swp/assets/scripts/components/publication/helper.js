cf18aeeb999393339194c86b5798489f9ad2b711
Fabian Niemann <niemann@cosmocode.de>
2021-01-28 13:05:35 +0100

    SWP-47: Translate publications label correctly

--- swp/assets/scripts/components/publication/helper.js
+++ swp/assets/scripts/components/publication/helper.js
@@ -1,9 +1,6 @@
-import _, {interpolate, ngettext} from 'utils/i18n';
+import {interpolate, ngettext} from 'utils/i18n';
 
-
-const PublicationLabel = _('%s Publication');
-const PublicationsLabel = _('%s Publications');
 
 export const getPublicationsLabel = count => interpolate(
-    ngettext(PublicationLabel, PublicationsLabel, count), [count], false,
+    ngettext('%s Publication', '%s Publications', count), [count], false,
 );
