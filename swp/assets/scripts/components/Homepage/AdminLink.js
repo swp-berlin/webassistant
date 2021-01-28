e549ada1a270f5dc96767365b5136869c0880fac
Fabian Niemann <niemann@cosmocode.de>
2021-01-28 18:12:10 +0100

    SWP-61: Use fa-tools as icon for admin link

--- swp/assets/scripts/components/Homepage/AdminLink.js
+++ swp/assets/scripts/components/Homepage/AdminLink.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMagic} from '@fortawesome/free-solid-svg-icons/faMagic';
+import {faTools} from '@fortawesome/free-solid-svg-icons/faTools';
 
 import _ from 'utils/i18n';
 
@@ -9,7 +9,7 @@ const Label = _('Admin');
 
 const AdminLink = ({className, ...props}) => (
     <a href="/admin/" className={classNames('text-center', 'p-10', 'hover:bg-gray-100', className)} {...props}>
-        <FontAwesomeIcon icon={faMagic} size="6x" />
+        <FontAwesomeIcon icon={faTools} size="6x" />
         <span className="block mt-2">
             {Label}
         </span>
