2056e106ea8b3dc6ae245f264b43622459cac3ce
Fabian Niemann <niemann@cosmocode.de>
2021-02-01 12:12:55 +0100

    SWP-47: Link publication in title if available

--- swp/assets/scripts/components/publication/PublicationList.js
+++ swp/assets/scripts/components/publication/PublicationList.js
@@ -24,10 +24,10 @@ const PDFNotFound = () => (
     </span>
 );
 
-export const PublicationItem = ({id, title, authors, abstract, publicationDate, pdfURL, pdfPages, ...props}) => (
+export const PublicationItem = ({id, title, authors, abstract, publicationDate, url, pdfURL, pdfPages, ...props}) => (
     <article className="publication-item mb-2" {...props}>
         <header>
-            <h5>{title}</h5>
+            <h5>{url ? <a href={url}>{title}</a> : title}</h5>
             <div className="subtitle">
                 <Authors authors={authors} />
                 <time className="ml-4">{publicationDate}</time>
@@ -44,7 +44,7 @@ export const PublicationItem = ({id, title, authors, abstract, publicationDate, 
 );
 
 const PublicationList = ({items, className, ...props}) => (
-    <ul className={classNames('publication-list', 'list-none', 'p-0', items.length > 0 || 'empty', className)} {...props}>
+    <ul className={classNames('publication-list', 'list-none', 'space-y-4', 'p-0', items.length > 0 || 'empty', className)} {...props}>
         {items.map(publication => (
             <li key={publication.id}>
                 <PublicationItem
@@ -53,6 +53,7 @@ const PublicationList = ({items, className, ...props}) => (
                     authors={publication.authors}
                     abstract={publication.abstract}
                     publicationDate={publication.publication_date}
+                    url={publication.url}
                     pdfURL={publication.pdf_url}
                     pdfPages={publication.pdf_pages}
                 />
