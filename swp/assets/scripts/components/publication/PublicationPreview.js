2b593bef430302883762400360722f383eba21dd
Fabian Niemann <niemann@cosmocode.de>
2021-02-02 11:02:30 +0100

    SWP-47: Refactor publication preview using Query component

--- swp/assets/scripts/components/publication/PublicationPreview.js
+++ swp/assets/scripts/components/publication/PublicationPreview.js
@@ -1,70 +1,59 @@
-import {useEffect, useState} from 'react';
 import {useLocation} from 'react-router-dom';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import {useQuery} from 'hooks/query';
 import _ from 'utils/i18n';
 
+import {Query} from 'components/Fetch';
 import {getPublicationsLabel} from './helper';
 import PublicationResults from './PublicationResults';
 
 
-const Loading = _('Loading');
 const NoPublications = _('No publications');
 
 const calculatePageCount = (total, pageSize) => Math.ceil(total / pageSize);
 
-const PublicationPreview = ({thinktankID, page, pageSize, noTitle, className, ...props}) => {
+const PublicationPreview = ({thinktankID, endpoint, page, pageSize, noTitle, className, ...props}) => {
     const location = useLocation();
-    const [currentPage, setCurrentPage] = useState(page || 1);
+    const search = new URLSearchParams(location.search);
+    const currentPage = page || +search.get('page') || 1;
 
-    useEffect(
-        () => {
-            const params = new URLSearchParams(location.search);
-            const pageParam = +params.get('page');
-
-            if (pageParam && pageParam !== currentPage) {
-                setCurrentPage(pageParam);
-            }
-        },
-        [location, currentPage, setCurrentPage],
-    );
-
-    const endpoint = '/publication/';
     const params = {thinktank_id: thinktankID, page: currentPage, page_size: pageSize};
-    const {loading, result} = useQuery(endpoint, params);
-
-    if (loading) return Loading;
-
-    const {data: {results, next: nextPage, previous: prevPage, count}} = result;
-    const title = count ? getPublicationsLabel(count) : NoPublications;
 
     return (
-        <div className={classNames('publication-preview', 'my-4', className)} {...props}>
-            {noTitle || <header className="mb-2"><h3>{title}</h3></header>}
+        <Query endpoint={endpoint} params={params}>
+            {({results, next: nextPage, previous: prevPage, count}) => (
+                <div className={classNames('publication-preview', 'my-4', className)} {...props}>
+                    {noTitle || (
+                        <header className="mb-2">
+                            <h3>{count ? getPublicationsLabel(count) : NoPublications}</h3>
+                        </header>
+                    )}
 
-            {count > 0 && (
-                <PublicationResults
-                    results={results}
-                    pageCount={calculatePageCount(count, pageSize)}
-                    currentPage={currentPage}
-                    setCurrentPage={setCurrentPage}
-                    nextPage={nextPage}
-                    prevPage={prevPage}
-                />
+                    {count > 0 && (
+                        <PublicationResults
+                            results={results}
+                            pageCount={calculatePageCount(count, pageSize)}
+                            currentPage={currentPage}
+                            nextPage={nextPage}
+                            prevPage={prevPage}
+                        />
+                    )}
+                </div>
             )}
-        </div>
+        </Query>
     );
 };
 
 PublicationPreview.defaultProps = {
+    endpoint: '/publication/',
     page: null,
-    pageSize: 10,
+    pageSize: 3,
     noTitle: false,
 };
 
 PublicationPreview.propTypes = {
     thinktankID: PropTypes.number.isRequired,
+    endpoint: PropTypes.string,
     page: PropTypes.number,
     pageSize: PropTypes.number,
     noTitle: PropTypes.bool,
--- swp/assets/scripts/components/publication/PublicationResults.js
+++ swp/assets/scripts/components/publication/PublicationResults.js
@@ -8,7 +8,7 @@ import PublicationList from './PublicationList';
 
 const generatePageNumbers = count => Array(count).fill(0).map((e, i) => i + 1);
 
-const PageButton = ({page, setCurrentPage, onClick, active, icon, disabled, ...props}) => {
+const PageButton = ({page, setCurrentPage, active, icon, disabled, ...props}) => {
     const location = useLocation();
     const search = new URLSearchParams(location.search);
     search.set('page', page);
@@ -17,10 +17,8 @@ const PageButton = ({page, setCurrentPage, onClick, active, icon, disabled, ...p
         search: search.toString(),
     };
 
-    const handleClick = useCallback(() => setCurrentPage(page), [page, setCurrentPage]);
-
     return (
-        <Link className={classNames('bp3-button', active && 'bp3-active', disabled && 'bp3-disabled')} to={to} onClick={onClick || handleClick} disabled={disabled} {...props}>
+        <Link className={classNames('bp3-button', active && 'bp3-active', disabled && 'bp3-disabled')} to={to} disabled={disabled} {...props}>
             {icon ? <Icon icon={icon} /> : page}
         </Link>
     );
@@ -30,16 +28,11 @@ const PageButtons = ({pages, currentPage, setCurrentPage}) => pages.map(page => 
     <PageButton key={page} page={page} setCurrentPage={setCurrentPage} active={page === currentPage} />
 ));
 
-const PublicationResults = ({results, pageCount, currentPage, setCurrentPage, nextPage, prevPage, ...props}) => {
+const PublicationResults = ({results, pageCount, currentPage, nextPage, prevPage, ...props}) => {
     const pages = useMemo(() => generatePageNumbers(pageCount), [pageCount]);
 
     const nextPageNumber = Math.min(currentPage + 1, pageCount);
     const prevPageNumber = Math.max(currentPage - 1, 1);
-
-    const handleNextPage = useCallback(() => nextPage && setCurrentPage(page => page + 1), [setCurrentPage, nextPage]);
-    const handlePrevPage = useCallback(() => prevPage && setCurrentPage(page => page - 1), [setCurrentPage, prevPage]);
-    const handleFirstPage = useCallback(() => setCurrentPage(1), [setCurrentPage]);
-    const handleLastPage = useCallback(() => setCurrentPage(pageCount), [setCurrentPage, pageCount]);
 
     return (
         <div className="publication-results">
@@ -47,11 +40,11 @@ const PublicationResults = ({results, pageCount, currentPage, setCurrentPage, ne
 
             <div className="pagination mt-4" key="pagination">
                 <ButtonGroup className="page-buttons">
-                    <PageButton key="first" page={1} disabled={!prevPage} onClick={handleFirstPage} icon="double-chevron-left" />
-                    <PageButton key="prev" page={prevPageNumber} onClick={handlePrevPage} disabled={!prevPage} icon="chevron-left" />
-                    <PageButtons pages={pages} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                    <PageButton key="next" page={nextPageNumber} onClick={handleNextPage} disabled={!nextPage} icon="chevron-right" />
-                    <PageButton key="last" page={pageCount} onClick={handleLastPage} disabled={!nextPage} icon="double-chevron-right" />
+                    <PageButton key="first" page={1} disabled={!prevPage} icon="double-chevron-left" />
+                    <PageButton key="prev" page={prevPageNumber} disabled={!prevPage} icon="chevron-left" />
+                    <PageButtons pages={pages} currentPage={currentPage} />
+                    <PageButton key="next" page={nextPageNumber} disabled={!nextPage} icon="chevron-right" />
+                    <PageButton key="last" page={pageCount} disabled={!nextPage} icon="double-chevron-right" />
                 </ButtonGroup>
             </div>
         </div>
