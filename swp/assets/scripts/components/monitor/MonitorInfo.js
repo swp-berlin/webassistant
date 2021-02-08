dc5d47bf35b1ac832e154a9b3e217d52d907b0f8
Fabian Niemann <niemann@cosmocode.de>
2021-02-08 15:48:47 +0100

    SWP-63: Include colons in translation messages

--- swp/assets/scripts/components/monitor/MonitorInfo.js
+++ swp/assets/scripts/components/monitor/MonitorInfo.js
@@ -3,45 +3,45 @@ import {getLabel} from 'utils/choices';
 import _ from 'utils/i18n';
 
 
-const PublicationCountLabel = _('Publications overall');
-const NewPublicationsLabel = _('Publications since last email');
-const LastSentLabel = _('Email sent');
-const IntervalLabel = _('Interval');
-const RecipientsLabel = _('Recipients');
+const PublicationCountLabel = _('Publications overall:');
+const NewPublicationsLabel = _('Publications since last email:');
+const LastSentLabel = _('Email sent:');
+const IntervalLabel = _('Interval:');
+const RecipientsLabel = _('Recipients:');
 
 
 const MonitorInfo = ({publicationCount, newPublicationCount, lastSent, interval, recipientCount, ...props}) => (
     <dl {...props}>
         <div className="mb-1 sm:grid sm:grid-cols-5 sm:gap-4">
-            <dt>{`${PublicationCountLabel}:`}</dt>
+            <dt>{PublicationCountLabel}</dt>
             <dd className="sm:col-span-4">
                 {publicationCount}
             </dd>
         </div>
 
         <div className="mb-1 sm:grid sm:grid-cols-5 sm:gap-4">
-            <dt>{`${NewPublicationsLabel}:`}</dt>
+            <dt>{NewPublicationsLabel}</dt>
             <dd className="sm:col-span-4">
                 {newPublicationCount}
             </dd>
         </div>
 
         <div className="mb-1 sm:grid sm:grid-cols-5 sm:gap-4">
-            <dt>{`${LastSentLabel}:`}</dt>
+            <dt>{LastSentLabel}</dt>
             <dd className="sm:col-span-4">
                 <DateTime value={lastSent} />
             </dd>
         </div>
 
         <div className="mb-1 sm:grid sm:grid-cols-5 sm:gap-4">
-            <dt>{`${IntervalLabel}:`}</dt>
+            <dt>{IntervalLabel}</dt>
             <dd className="sm:col-span-4">
                 {interval ? getLabel('interval', interval) : '—'}
             </dd>
         </div>
 
         <div className="sm:grid sm:grid-cols-5 sm:gap-4">
-            <dt>{`${RecipientsLabel}:`}</dt>
+            <dt>{RecipientsLabel}</dt>
             <dd className="sm:col-span-4">
                 {recipientCount}
             </dd>
