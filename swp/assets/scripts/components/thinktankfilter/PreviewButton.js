676a9471276fdb9c2629ff5b7095bd4be1971385
Benjamin Brauch <brauch@cosmocode.de>
2021-02-16 10:56:09 +0100

    added thinktank filter preview error handling

--- swp/assets/scripts/components/thinktankfilter/PreviewButton.js
+++ swp/assets/scripts/components/thinktankfilter/PreviewButton.js
@@ -2,13 +2,21 @@ import {useCallback, useEffect} from 'react';
 import {Button} from '@blueprintjs/core';
 
 import _ from 'utils/i18n';
-import {useMutation} from 'hooks/query';
+import {useMutationResult} from 'components/Fetch';
+import {setErrors} from 'utils/form';
 
 
 const ButtonLabel = _('Preview');
 
 const PreviewButton = ({form, onPreview}) => {
-    const [mutate, {loading, success, result}] = useMutation('/thinktankfilter/preview/');
+    const [mutate, {loading, success, result}] = useMutationResult(
+        '/thinktankfilter/preview/',
+        {
+            method: 'POST',
+            handleSuccess: () => {},
+            setErrors: errors => setErrors(form.setError, errors),
+        },
+    );
 
     const handleClick = useCallback(async () => {
         const valid = await form.trigger();
