6b38c9183da7597195b1fcdc10e2095d16fe6ffc
Fabian Niemann <niemann@cosmocode.de>
2021-03-12 16:28:12 +0100

    SWP-125: Show monitor active state in list view

--- swp/assets/scripts/components/monitor/MonitorRow.js
+++ swp/assets/scripts/components/monitor/MonitorRow.js
@@ -1,5 +1,9 @@
 import {Link} from 'react-router-dom';
 
+import DateTime from 'components/DateTime';
+import _ from 'utils/i18n';
+
+const DisabledLabel = _('disabled');
 
 const MonitorLink = ({id, children, ...props}) => (
     <Link to={`/monitor/${id}/`} {...props}>
@@ -7,12 +11,13 @@ const MonitorLink = ({id, children, ...props}) => (
     </Link>
 );
 
-const ThinktankRow = ({id, name, recipientCount, publicationCount, newPublicationCount}) => (
-    <tr>
+const ThinktankRow = ({id, name, recipientCount, publicationCount, newPublicationCount, lastSent, isActive}) => (
+    <tr className={isActive ? '' : 'disabled'}>
         <td><MonitorLink id={id}>{name}</MonitorLink></td>
         <td className="text-right">{recipientCount}</td>
         <td className="text-right">{publicationCount}</td>
         <td className="text-right">{newPublicationCount}</td>
+        <td className="text-right">{isActive ? <DateTime value={lastSent} /> : DisabledLabel}</td>
     </tr>
 );
 
