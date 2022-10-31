e293bb501e50544435e80857a006cc3374f47d33
Oliver Zander <zander@cosmocode.de>
2022-10-31 11:55:49 +0100

    SWP-201: fixed task import

--- swp/tasks/__init__.py
+++ swp/tasks/__init__.py
@@ -4,7 +4,7 @@ from .monitor import (
     send_all_monitor_publications_to_zotero,
     send_publications_to_zotero,
 )
-from monitoring import monitoring
+from .monitoring import monitoring
 from .publicationcount import update_publication_count
 from .scraper import preview_scraper
 from .scheduling import run_scraper, schedule_scrapers
