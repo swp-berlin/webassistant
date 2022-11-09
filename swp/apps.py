4184cfd200082f7f4dcbfa4b677c325ff497797d
Oliver Zander <zander@cosmocode.de>
2022-11-09 13:30:26 +0100

    updated django to 3.2

--- swp/apps.py
+++ swp/apps.py
@@ -5,7 +5,9 @@ from django.contrib.admin.apps import AdminConfig as DefaultAdminConfig
 class SWPConfig(AppConfig):
     name = 'swp'
     verbose_name = 'SWP'
+    default_auto_field = 'django.db.models.AutoField'
 
 
 class AdminConfig(DefaultAdminConfig):
     default_site = 'swp.site.AdminSite'
+    default = False
