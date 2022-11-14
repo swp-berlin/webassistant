3af80f2fc70054659524447efa369e773d5e3d04
Benjamin Brauch <brauch@cosmocode.de>
2022-11-14 15:13:15 +0100

    DJAN-213: show selected date range in date picker icon

--- swp/assets/scripts/components/Search/DateRangePicker.js
+++ swp/assets/scripts/components/Search/DateRangePicker.js
@@ -3,16 +3,41 @@ import {DateRangePicker as BPDateRangePicker} from '@blueprintjs/datetime';
 import {Classes, Popover2} from '@blueprintjs/popover2';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCalendar} from '@fortawesome/free-solid-svg-icons/faCalendar';
+import format from 'date-fns/format';
 
-const DateRangePicker = ({defaultValue, onChange}) => (
-    <Popover2
-        interactionKind="click"
-        popoverClassName={Classes.POPOVER2_CONTENT_SIZING}
-        placement="bottom-start"
-        content={<BPDateRangePicker defaultValue={defaultValue} onChange={onChange} />}
-    >
-        <Button minimal icon={<FontAwesomeIcon icon={faCalendar} />} />
-    </Popover2>
-);
+import _ from 'utils/i18n';
+import {useState} from 'react';
+
+const SinceLabel = _('since');
+const UntilLabel = _('until');
+
+const formatDate = date => date && format(date, 'dd.MM.yyyy', {representation: 'date'});
+
+const getDateRepresentation = ([startDate, endDate]) => {
+    if (!startDate && !endDate) return '';
+    if (startDate && !endDate) return `${SinceLabel} ${formatDate(startDate)}`;
+    if (endDate && !startDate) return `${UntilLabel} ${formatDate(endDate)}`;
+
+    return `${formatDate(startDate)} ${UntilLabel} ${formatDate(endDate)}`;
+};
+
+const DateRangePicker = ({defaultValue, onChange}) => {
+    const [dates, setDates] = useState([null, null]);
+    const handleChange = dates => {
+        setDates(dates);
+        onChange(dates);
+    };
+
+    return (
+        <Popover2
+            interactionKind="click"
+            popoverClassName={Classes.POPOVER2_CONTENT_SIZING}
+            placement="bottom-start"
+            content={<BPDateRangePicker defaultValue={defaultValue} onChange={handleChange} />}
+        >
+            <Button minimal icon={<FontAwesomeIcon icon={faCalendar} />} text={getDateRepresentation(dates)} />
+        </Popover2>
+    );
+};
 
 export default DateRangePicker;
