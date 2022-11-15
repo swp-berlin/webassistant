91fc691ea384060178de16bb305e4fef3ed6f306
Oliver Zander <zander@cosmocode.de>
2022-11-15 11:09:04 +0100

    added missing trailing slashes

--- swp/assets/scripts/components/RootURLConfig.js
+++ swp/assets/scripts/components/RootURLConfig.js
@@ -19,10 +19,10 @@ const RootURLConfig = () => (
     <Routes>
         <Route path="/" element={<Homepage />} />
 
-        <Route path="/monitor" element={<MonitorList />} />
-        <Route path="/monitor/:id" element={<MonitorDetail />} />
-        <Route path="/monitor/:id/edit" element={<MonitorEdit />} />
-        <Route path="/monitor/add" element={<MonitorAdd />} />
+        <Route path="/monitor/" element={<MonitorList />} />
+        <Route path="/monitor/:id/" element={<MonitorDetail />} />
+        <Route path="/monitor/:id/edit/" element={<MonitorEdit />} />
+        <Route path="/monitor/add/" element={<MonitorAdd />} />
         <Route path="/monitor/:id/filter/add/" element={<ThinktankFilterAdd />} />
         <Route path="/monitor/:monitorID/filter/:id/edit/" element={<ThinktankFilterEdit />} />
         <Route path="/monitor/:id/publications/" element={<MonitorPublications />} />
@@ -33,7 +33,7 @@ const RootURLConfig = () => (
         <Route path="/thinktank/:id/" element={<ThinktankDetail />} />
         <Route path="/thinktank/:id/edit/" element={<ThinktankEdit />} />
         <Route path="/thinktank/:id/publications/" element={<ThinktankPublications />} />
-        <Route path="/thinktank/:thinktankID/scraper/add" element={<ScraperAdd />} />
+        <Route path="/thinktank/:thinktankID/scraper/add/" element={<ScraperAdd />} />
         <Route path="/thinktank/:thinktankID/scraper/:id/" element={<ScraperEdit />} />
 
         <Route path="/search/" element={<SearchPage />} />
