ca0150cd1a8f8c7b82827c436595920b609a8816
Oliver Zander <zander@cosmocode.de>
2023-12-06 11:13:33 +0100

    unified thinktank serializers

--- swp/api/serializers/thinktank.py
+++ swp/api/serializers/thinktank.py
@@ -8,62 +8,51 @@ from swp.models import Thinktank
 from .scraper import ScraperListSerializer
 
 
-class ThinktankSerializer(ModelSerializer):
+class BaseThinktankSerializer(ModelSerializer):
+    """
+    Base thinktank serializer.
+    """
+
+    can_manage = BooleanField(read_only=True)
+
+    class Meta:
+        model = Thinktank
+        read_only_fields = [
+            'can_manage',
+            'last_run',
+            'created',
+            'publication_count',
+            'scraper_count',
+            'active_scraper_count',
+            'last_error_count',
+        ]
+        fields = [
+            'id',
+            'name',
+            'url',
+            'is_active',
+            'unique_fields',
+            *read_only_fields,
+        ]
+
+
+class ThinktankSerializer(BaseThinktankSerializer):
     """
     Full thinktank serializer.
     """
 
     scrapers = ScraperListSerializer(many=True, read_only=True)
+    is_active = BooleanField(label=_('active'), required=False)
 
-    can_manage = BooleanField(read_only=True)
-    is_active = BooleanField(label=_('Active'), required=False)
-
-    class Meta:
-        model = Thinktank
-        read_only_fields = [
-            'last_run',
-            'created',
-            'publication_count',
-            'scraper_count',
-            'active_scraper_count',
-            'last_error_count',
-            'scrapers',
-            'can_manage',
-        ]
-        fields = [
-            'id',
-            'name',
-            'description',
-            'url',
-            'unique_fields',
-            'is_active',
-            *read_only_fields,
-        ]
+    class Meta(BaseThinktankSerializer.Meta):
+        read_only_fields = [*BaseThinktankSerializer.Meta.read_only_fields, 'scrapers']
+        fields = [*BaseThinktankSerializer.Meta.fields, 'description', 'scrapers']
 
 
-class ThinktankListSerializer(ModelSerializer):
+class ThinktankListSerializer(BaseThinktankSerializer):
     """
     Light serializer for thinktank lists.
     """
 
-    can_manage = BooleanField(read_only=True)
-
-    class Meta:
-        model = Thinktank
-        read_only_fields = [
-            'is_active',
-            'last_run',
-            'created',
-            'publication_count',
-            'scraper_count',
-            'active_scraper_count',
-            'last_error_count',
-            'can_manage',
-        ]
-        fields = [
-            'id',
-            'name',
-            'url',
-            'unique_fields',
-            *read_only_fields,
-        ]
+    class Meta(BaseThinktankSerializer.Meta):
+        read_only_fields = [*BaseThinktankSerializer.Meta.read_only_fields, 'is_active']
