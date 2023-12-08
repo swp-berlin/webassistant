1633e033f28a9f8cccbab56c2c9d2146306c6d97
Oliver Zander <zander@cosmocode.de>
2023-12-08 14:39:31 +0100

    clean up scraper form

--- swp/assets/scripts/components/scraper/ScraperActivationButton.js
+++ swp/assets/scripts/components/scraper/ScraperActivationButton.js
@@ -2,38 +2,38 @@ import {useEffect} from 'react';
 import {Button, Intent} from '@blueprintjs/core';
 
 import _ from 'utils/i18n';
-import {useMutationResult} from 'components/Fetch';
 import {setErrors} from 'utils/form';
+
+import {useMutationResult} from 'components/Fetch';
 
 const ActivateLabel = _('Activate');
 const DeactivateLabel = _('Deactivate');
 const ActivatedMessage = _('Scraper has been activated.');
 const DeactivatedMessage = _('Scraper has been deactivated.');
 
+const handleSuccess = ({is_active: isActive}) => ({
+    intent: 'success',
+    message: isActive ? ActivatedMessage : DeactivatedMessage,
+});
 
 const ScraperActivationButton = ({id, isActive, form, onToggle}) => {
-    const [mutate, {success, result: {data}}] = useMutationResult(
-        `/scraper/${id}/activate/`,
-        {
-            handleSuccess: ({is_active: isActive}) => ({
-                intent: 'success',
-                message: isActive ? ActivatedMessage : DeactivatedMessage,
-            }),
-            method: 'POST',
-            setErrors: errors => setErrors(form.setError, errors),
+    const endpoint = `/scraper/${id}/activate/`;
+    const mutationOptions = {
+        method: 'POST',
+        handleSuccess,
+        setErrors(errors) {
+            setErrors(form.setError, errors);
         },
-        [],
-    );
-
-    const handleClick = form.handleSubmit(async data => {
-        await mutate({...data, is_active: !isActive});
-    });
+    };
+    const [mutate, {success, result: {data}, loading}] = useMutationResult(endpoint, mutationOptions, []);
+    const handleClick = form.handleSubmit(data => mutate({...data, is_active: !isActive}));
+    const label = isActive ? DeactivateLabel : ActivateLabel;
 
     useEffect(() => {
         if (success) onToggle(data.is_active);
     }, [data, onToggle, success]);
 
-    return <Button intent={Intent.PRIMARY} onClick={handleClick} text={isActive ? DeactivateLabel : ActivateLabel} />;
+    return <Button intent={Intent.PRIMARY} text={label} loading={loading} onClick={handleClick} />;
 };
 
 export default ScraperActivationButton;
