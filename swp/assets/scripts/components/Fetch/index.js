3bab2fa628c485c1a249c58daa7fb9e8be0e1bd4
Oliver Zander <zander@cosmocode.de>
2025-02-24 17:49:34 +0100

    SWP-289: add permission error when trying to edit monitor without pool perm

--- swp/assets/scripts/components/Fetch/index.js
+++ swp/assets/scripts/components/Fetch/index.js
@@ -3,9 +3,20 @@ import Query from './Query';
 import Result from './Result';
 import Loading from './Loading';
 import ChoicesQuery from './ChoicesQuery';
+import ClientError from './ClientError';
 import NetworkError from './NetworkError';
 
-export {Form, Query, Result, Loading, ChoicesQuery, NetworkError, useMutationForm, useMutationResult};
+export {
+    Form,
+    Query,
+    Result,
+    Loading,
+    ChoicesQuery,
+    ClientError,
+    NetworkError,
+    useMutationForm,
+    useMutationResult,
+};
 
 export default {
     Form,
