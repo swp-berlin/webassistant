6e512055385d75ea0b3e04601a7827b076a23e5b
Oliver Zander <zander@cosmocode.de>
2025-11-05 15:56:42 +0100

    SWP-302: do not allow active scrapers to be changed

--- swp/api/v1/scraper/viewsets.py
+++ swp/api/v1/scraper/viewsets.py
@@ -1,6 +1,7 @@
 from swp.api.v1.viewsets import ActivatableViewSet
 from swp.models import Scraper
 
+from .exceptions import ScraperActiveException
 from .filters import ScraperFilterSet
 from .serializers import ScraperSerializer
 
@@ -10,3 +11,9 @@ class ScraperViewSet(ActivatableViewSet):
     serializer_class = ScraperSerializer
     filterset_class = ScraperFilterSet
     queryset = Scraper.objects
+
+    def check_object_permissions(self, request, obj: Scraper):
+        super().check_object_permissions(request, obj)
+
+        if self.action in {'update', 'partial_update'} and obj.is_active:
+            raise ScraperActiveException
