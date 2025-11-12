8e47058a661bc7191469f13bca825641302a1fdc
Oliver Zander <zander@cosmocode.de>
2025-11-12 11:35:16 +0100

    SWP-302: validate active monitor has query

--- swp/api/v1/monitor/serializers.py
+++ swp/api/v1/monitor/serializers.py
@@ -9,3 +9,21 @@ class MonitorSerializer(ActivatableSerializer):
     class Meta:
         model = Monitor
         fields = serializers.ALL_FIELDS
+
+    def validate(self, attrs):
+        self.validate_active_monitor_has_query(attrs)
+
+        return attrs
+
+    def validate_active_monitor_has_query(self, attrs):
+        instance = self.instance
+
+        if instance is None:
+            return None
+
+        instance = Monitor(
+            query=attrs.get('query', instance.query),
+            is_active=self.activate or instance.is_active,
+        )
+
+        return Monitor.clean(instance)
